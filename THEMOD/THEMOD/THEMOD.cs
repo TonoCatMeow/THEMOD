527647c6c62c1190defd25fda5c98a247e50b18b
TonoCatMeow <86448220+TonoCatMeow@users.noreply.github.com>
2022-01-21 21:27:57 -0500

    Add files via upload

---
+++ THEMOD/THEMOD/THEMOD.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Testing : Mod
+{
+    public void Start()
+    {
+        Debug.Log("THE MOD HAS LOADED! WOOOOOO!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("The mod is unloaded :(.");
+    }
+}
